ab325292b12d6d5b27d69655472c1c5fe90c95b8
GuillaumeLeGoff <legoff.guill@gmail.com>
2025-02-05 14:38:39 +0100

    fix temperature preview

--- server/preview/src/components/AutoTemperatureComponent.jsx
+++ server/preview/src/components/AutoTemperatureComponent.jsx
@@ -1,24 +1,15 @@
 // @AutoTemperatureComponent.jsx
 
 import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
-import { useState, useEffect } from "react";
 import useSocketData from "../stores/socketDataStore";
 
 const AutoTemperatureComponent = () => {
-  const [temperature, setTemperature] = useState("20°C");
-
   const { socketData } = useSocketData();
   // Simulate fetching temperature from an API
 
-  useEffect(() => {
-    const temperature = socketData?.temperature;
-    console.log(temperature);
-    setTemperature(temperature + "°C");
-  }, [socketData]);
-
   return (
     <NodeViewWrapper className="auto-date-component">
-      <span className="label">{temperature}</span>
+      <span className="label">{socketData?.temperature || "---"}°C</span>
       <NodeViewContent className="content" />
     </NodeViewWrapper>
   );
